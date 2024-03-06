c0fe861a8e14d44dfbac2e2a34006744c3ee807f
malcolmrodgers <rodgers.mlclm@gmail.com>
2024-03-06 04:15:26 -0500

    Started whoWon method

--- PlayGame.py
+++ PlayGame.py
@@ -1,7 +1,22 @@
 import numpy as np
 import random as rand
 from Game import Game
-
+def whoWon(a, b):
+    winner = bool
+    if a == 'R' and b == 'S':
+        winner = True
+    elif a == 'R' and b == 'P':
+        winner = False
+    elif a == 'P' and b == 'R':
+        winner = True
+    elif a == 'P' and b == 'S':
+        winner = False
+    elif a == 'S' and b == 'P':
+        winner = True
+    elif a == 'S' and b == 'R':
+        winner = False
+    elif a == b:
+        return "Tie"
 
 def playGame():
     newGame = Game(0, 0, '', '')
